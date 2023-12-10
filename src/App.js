a04dc6dd9b02d9b609f8d926448a2b4024f3aa93
Mohamadi12 <adilnana78@gmail.com>
2023-12-10 18:06:27 +0100

    deleteadd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [foods, setFoods] = useState(["Apple", "Orange", "Banana"]);
+  const [newFood, setNewFood] = useState(""); // Ajout d'un état pour stocker le nouvel aliment
+
+  function handleAddFodd() {
+    if (newFood.trim() !== "") {
+      setFoods([...foods, newFood]); // Ajouter le nouvel aliment à la liste existante
+      setNewFood(""); // Réinitialiser le champ pour le nouvel aliment
+    }
+  }
+
+  function handleRemoveFodd(index) {
+    setFoods(foods.filter((_, i) => i !== index));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>List of food</h2>
+      <ul>
+        {foods.map((food, index) => (
+          <li key={index} onClick={() => handleRemoveFodd(index)}>
+            {food}
+          </li>
+        ))}
+      </ul>
+      <input
+        type="text"
+        placeholder="Enter food name"
+        value={newFood}
+        onChange={(e) => setNewFood(e.target.value)}
+      />
+      <button onClick={handleAddFodd}>Add Food</button>
     </div>
   );
 }
